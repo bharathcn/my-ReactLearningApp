989cf72940bc12331ec049dad75eefa13c021a01
bharathcn <54095449+bharathcn@users.noreply.github.com>
2021-09-27 10:50:09 +0530

    Updated review changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-function App() {
+import React from 'react';
+import Parent from './ParentComponent';
+const App = () =>{
   return (
+    <React.StrictMode>
     <div>
         <p>
           Example of Functional Component.
         </p>
+        <Parent/>
     </div>
+    </React.StrictMode>
   );
 }
 
